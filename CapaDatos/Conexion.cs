94752507a87616f4b1595b4bfab8248bc9cb7969
marlon san juan <marlon.sanjuan@teleticket.com.pe>
2024-06-26 20:36:47 -0500

    Agregar archivos de proyecto.

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    class Conexion
+    {
+        //
+        // Declaramos las variables privadas de esta clase
+        //
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private bool Seguridad; // nos permitira establecer el metodo de autenticacion a sql
+        private static Conexion con = null;  // creamos un objeto de tipo conexion que es el que vamos a compartir
+
+        private Conexion()
+        {
+            this.Base = "BDUSERS";
+            this.Servidor = "TLK1762-MARLONA\\SQLEXPRESS";
+            this.Usuario = "sa";
+            this.Clave = "1802Maximo";
+            this.Seguridad = true;
+        }
+
+        // Generamos un metodo publico para generar el string de conexion
+        public SqlConnection crearConexion()
+        {
+            // declaramos una variable de tipo sqlConection
+            SqlConnection cadena = new SqlConnection();
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
